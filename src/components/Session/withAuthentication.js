9caa330fa48f326619f804940aedd99b4bcafa05
Khalid <me@khalidragi.com>
2019-12-18 21:12:47 +0300

    added merging Auth and Authori to firebase class as method

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -10,11 +10,14 @@ const withAuthentication = Component => {
       this.state = { authUser: null };
     }
     componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
-        authUser
-          ? this.setState({ authUser })
-          : this.setState({ authUser: null });
-      });
+      this.listener = this.props.firebase.onAuthUserListener(
+        authUser => {
+          this.setState({ authUser });
+        },
+        () => {
+          this.setState({ authUser: null });
+        }
+      );
     }
     componentWillUnmount() {
       this.listener();
