c5d89eb37070c7b7e2f92bdaa53767fd1c321f22
Khalid <me@khalidragi.com>
2019-12-19 02:21:21 +0300

    link sign-in with social accounts to one account

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -15,6 +15,8 @@ const config = {
 class Firebase {
   constructor() {
     app.initializeApp(config);
+
+    this.emailAuthProvider = app.auth.EmailAuthProvider;
     this.auth = app.auth();
     this.db = app.database();
     this.googleProvider = new app.auth.GoogleAuthProvider();
